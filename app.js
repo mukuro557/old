a73a79b4311e2283315c71683a824bdb19124cc2
mukuro557 <56430402+mukuro557@users.noreply.github.com>
2019-12-09 21:19:08 +0700

    cancel join

--- app.js
+++ app.js
@@ -40,9 +40,9 @@ app.post("/login", function (req, res) {
         else {
             const numrows = result.length;
             console.log(numrows)
-            
+
             if (numrows != 1) {
-               
+
                 res.status(401).send("Wrong username");
             }
             else {
@@ -51,13 +51,13 @@ app.post("/login", function (req, res) {
                         res.status(503).send("Authentication Server error");
                     }
                     else if (resp == true) {
-                        
+
                         if (result[0].role == 1) {
-                            
+
                             res.send("/admin");
                         }
                         else {
-                            
+
                             res.send("/welcome");
                         }
                     }
@@ -76,20 +76,20 @@ app.post("/login", function (req, res) {
 
 // <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< signup >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 //------------------------------- Sign up a new user -------------------------
-app.post("/signUp", function(req, res) {
+app.post("/signUp", function (req, res) {
     const username = req.body.username;
     const password = req.body.password;
 
     //encrypt password
     const saltRounds = 10;    //the cost of encrypting
 
-    bcrypt.hash(password, saltRounds, function(err, hash) {
-        if(err) {
+    bcrypt.hash(password, saltRounds, function (err, hash) {
+        if (err) {
             console.error(err.message);
             res.status(500).send("Server error");
             return;
         }
-        
+
         //hash OK, do the rest here because of async hashing process!
         // insert new user (role is 2)
         const sql = "INSERT INTO login(username, password, role) VALUES(?,?,2)";
@@ -98,12 +98,12 @@ app.post("/signUp", function(req, res) {
                 console.error(err.message);
                 res.status(503).send("Database server error");
                 return;
-            }           
-            
+            }
+
             // get inserted rows
             const numrows = result.affectedRows;
-            if(numrows != 1) {
-                console.error("Insert to DB failed");                
+            if (numrows != 1) {
+                console.error("Insert to DB failed");
                 res.status(503).send("Database server error");
             }
             else {
@@ -116,12 +116,12 @@ app.post("/signUp", function(req, res) {
 
 // <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< saveaddhealth >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
-app.post("/save_add_health", function(req, res){
+app.post("/save_add_health", function (req, res) {
     const date = req.body.date;
     const information = req.body.information;
 
     const sql = "INSERT INTO announcement(date, information) VALUES (?,?)";
-    con.query(sql, [date,information], function (err, result, fields) {
+    con.query(sql, [date, information], function (err, result, fields) {
         if (err) {
             console.error(err.message);
             res.status(500).send("เซิร์ฟเวอร์ไม่ตอบสนอง");
@@ -129,28 +129,28 @@ app.post("/save_add_health", function(req, res){
         }
         // get inserted rows
         const numrows = result.affectedRows;
-        if(numrows != 1) {
-            console.error("Insert to DB failed");                
+        if (numrows != 1) {
+            console.error("Insert to DB failed");
             res.status(503).send("Database server error");
         }
         else {
             res.send("Sign up Complete!");
-        }           
-        
+        }
+
     });
 });
 
 
 // <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< save_edit_anoucement >>>>>>>>>>>>>>>>>>>>>>>>>>
 
-app.put("/save_edit_announce", function(req, res){
+app.put("/save_edit_announce", function (req, res) {
     const date = req.body.date;
     const img = req.body.img;
     const title = req.body.title;
     const information = req.body.information;
 
     const sql = "INSERT INTO cardinfo(information, img, date, title) VALUES(?,?,?,?)";
-    con.query(sql, [information, img,  date, title], function (err, result, fields) {
+    con.query(sql, [information, img, date, title], function (err, result, fields) {
         if (err) {
             console.error(err.message);
             res.status(500).send("เซิร์ฟเวอร์ไม่ตอบสนอง");
@@ -158,14 +158,14 @@ app.put("/save_edit_announce", function(req, res){
         }
         // get inserted rows
         const numrows = result.affectedRows;
-        if(numrows != 1) {
-            console.error("Insert to DB failed");                
+        if (numrows != 1) {
+            console.error("Insert to DB failed");
             res.status(503).send("Database server error");
         }
         else {
             res.send("Sign up Complete!");
-        }           
-        
+        }
+
     });
 });
 
@@ -174,76 +174,76 @@ app.put("/save_edit_announce", function(req, res){
 //<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< Root >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 app.get("/", function (req, res) {
     res.sendFile(path.join(__dirname, "mainpage.html"));
- });
-
- //<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< visitmainpage>>>>>>>>>>>>>>>>>>>>>>>>>>>>
- app.get("/mainpage", function (req, res) {
-    let sql = "SELECT a.information,a.date,y.img,y.information,c.img, c.title,c.information FROM annoucement a, activity y,cardinfo c"
-con 
 });
 
- //<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<  readmore >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
- app.get("/readmore", function (req, res) {
+//<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< visitmainpage>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+app.get("/mainpage", function (req, res) {
+    let sql = "SELECT a.information,a.date,y.img,y.information,c.img, c.title,c.information FROM annoucement a, activity y,cardinfo c"
+    con
+});
+
+//<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<  readmore >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+app.get("/readmore", function (req, res) {
     let sql = "SELECT information,img, title FROM cardinfo "
- });
+});
 
- //<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< visit activity >>>>>>>>>>>>>>>>>>>>>>>>>>
- app.get("/activity", function (req, res) {
+//<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< visit activity >>>>>>>>>>>>>>>>>>>>>>>>>>
+app.get("/activity", function (req, res) {
     res.sendFile(path.join(__dirname, "activity.html"));
- });
+});
 
- //<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< show information >>>>>>>>>>>>>>>>>>>>>>>>
- app.get("/display_activity", function (req, res) {
+//<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< show information >>>>>>>>>>>>>>>>>>>>>>>>
+app.get("/display_activity", function (req, res) {
     let sql = "SELECT `activity_name`, `organizer`, `information`, `limit_join`, `date`, `number_join`  FROM `activity` "
- });
+});
 
- //<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< visit profile  >>>>>>>>>>>>>>>>>>>>>>>>>>
- app.get("/display_profile", function (req, res) {
-    let sql ="SELECT l.username,l.img,l.email,o.address,o.tel FROM login l, old_info o WHERE l.id_login =o.id_login"
- });
+//<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< visit profile  >>>>>>>>>>>>>>>>>>>>>>>>>>
+app.get("/display_profile", function (req, res) {
+    let sql = "SELECT l.username,l.img,l.email,o.address,o.tel FROM login l, old_info o WHERE l.id_login =o.id_login"
+});
 
- //<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< visit user profile >>>>>>>>>>>>>>>>>>>>>>>
- app.get("/user_profile", function (req, res) {
+//<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< visit user profile >>>>>>>>>>>>>>>>>>>>>>>
+app.get("/user_profile", function (req, res) {
     res.sendFile(path.join(__dirname, "user_profile.html"));
- });
+});
 
- //<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< oldinfo page >>>>>>>>>>>>>>>>>>>>>>>>>>>
- app.get("/confirmad", function (req, res) {
+//<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< oldinfo page >>>>>>>>>>>>>>>>>>>>>>>>>>>
+app.get("/confirmad", function (req, res) {
     res.sendFile(path.join(__dirname, "oldinfo.html"));
- });
+});
 
- //<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< display info in>>>>>>>>>>>>>>>>>>>>>>>>>
- app.get("/display_confirmad", function (req, res) {
-   
- });
+//<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< display info in>>>>>>>>>>>>>>>>>>>>>>>>>
+app.get("/display_confirmad", function (req, res) {
 
- //<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< display carousel >>>>>>>>>>>>>>>>>>>>>>>>
- app.get("/display_carouselEdit", function (req, res) {
-    
- });
+});
 
- //<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< visit health page >>>>>>>>>>>>>>>>>>>>>>>>>
- app.get("/health", function (req, res) {
+//<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< display carousel >>>>>>>>>>>>>>>>>>>>>>>>
+app.get("/display_carouselEdit", function (req, res) {
+
+});
+
+//<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< visit health page >>>>>>>>>>>>>>>>>>>>>>>>>
+app.get("/health", function (req, res) {
     res.sendFile(path.join(__dirname, "health.html"));
- });
+});
 
- //<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< display_health >>>>>>>>>>>>>>>>>>>>>>>>>>>>
- app.get("/display_health", function (req, res) {
-   
- });
+//<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< display_health >>>>>>>>>>>>>>>>>>>>>>>>>>>>
+app.get("/display_health", function (req, res) {
 
- //<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< mainpage admin >>>>>>>>>>>>>>>>>>>>>>>>>>>>
- app.get("/mainpage_admin", function (req, res) {
+});
+
+//<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< mainpage admin >>>>>>>>>>>>>>>>>>>>>>>>>>>>
+app.get("/mainpage_admin", function (req, res) {
     res.sendFile(path.join(__dirname, "mainpage_admin.html"));
- });
+});
 
- //<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< display mainpage admin>>>>>>>>>>>>>>>>>>>>
- app.get("/display_mainpage_admin", function (req, res) {
-   
- });
-  
- //<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<   editoldinfo   >>>>>>>>>>>>>>>>>>>>>>>>>>>
- app.put("/editoldinfo", function (req, res) {
+//<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< display mainpage admin>>>>>>>>>>>>>>>>>>>>
+app.get("/display_mainpage_admin", function (req, res) {
+
+});
+
+//<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<   editoldinfo   >>>>>>>>>>>>>>>>>>>>>>>>>>>
+app.put("/editoldinfo", function (req, res) {
     const name = req.body.name;
     const IDcard = req.body.IDcard;
     const Address = req.body.Address;
@@ -251,74 +251,74 @@ con
     const Mobilephone = req.body.Mobilephone;
     const Symptom = req.body.Symptom;
     const Allergic = req.body.Allergic;
-  
+
     const sql = "update user set name = ?,IDcard = ?,Address = ?,Emergencycall = ?,Mobilephone =?,Symptom =?,Allergic = ? where = ?";
-  
+
     con.query(sql, [name, IDcard, Address, Emergencycall, Mobilephone, Symptom, Allergic], function () {
     });
- });
- //<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<  joinactivity  >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
- app.put("/joinactivity", function (req, res) {
+});
+//<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<  joinactivity  >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+app.put("/joinactivity", function (req, res) {
     const id_old = req.body.id_old;
     const id_activity = req.body.id_activity;
-  
+
     const spl = "update user set id_old = ?,id_activity = ? where = ?";
-  
+
     con.query(sql, [id_old, id_activity], function () {
     });
- });
- //<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<  uploading_profile  >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
- app.put("/uploadimg_profile", function (req, res) {
+});
+//<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<  uploading_profile  >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+app.put("/uploadimg_profile", function (req, res) {
     const img = req.body.img;
-  
+
     const spl = "update user set img = ? where = ?";
-  
+
     con.query(sql, [img], function () {
     });
- });
- //<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<  Edit_information_of_profile  >>>>>>>>>>>>>>>>>>>
- app.put("/Edit_pofileinfo", function (req, res) {
+});
+//<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<  Edit_information_of_profile  >>>>>>>>>>>>>>>>>>>
+app.put("/Edit_pofileinfo", function (req, res) {
     const name = req.body.name;
     const tel = req.body.tel;
     const email = req.body.email;
     const address = req.body.address;
-  
+
     const spl = "update user set name = ?, tel = ?, email = ?, address = ? where = ?";
-  
-    con.query(sql, [name,tel,email,address], function () {
+
+    con.query(sql, [name, tel, email, address], function () {
     });
- });
- //<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<  approveinfo  >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
- app.put("/approveinfo", function (req, res) {
+});
+//<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<  approveinfo  >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+app.put("/approveinfo", function (req, res) {
     const date = req.body.date;
     const name = req.body.name;
     const surname = req.body.surname;
     const id_card = req.body.id_card;
-    const address = req.body.address;         
+    const address = req.body.address;
     const id_login = req.body.id_login;
-  
+
     const spl = "update user set date = ?,name = ?,surname = ?,id_card = ?,address = ?,id_login =:? ";
-  
-    con.query(sql, [date,name,surname,id_card , address,id_login ], function () {
+
+    con.query(sql, [date, name, surname, id_card, address, id_login], function () {
     });
- });
- //<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<  save_approveinfo  >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
- app.put("/save_approveinfo", function (req, res) {
+});
+//<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<  save_approveinfo  >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+app.put("/save_approveinfo", function (req, res) {
     const date = req.body.date;
     const name = req.body.name;
     const surname = req.body.surname;
     const id_card = req.body.id_card;
     const address = req.body.address;
     const id_login = req.body.id_loginl;
-  
-    const spl = "update user set date = ?,name = ?,surname = ?,id_card = ?,address = ?,id_login =:? ";
-  
-    con.query(sql, [date,name,surname,id_card ,address,id_login ], function () {
-    });
- });
- 
 
- // const date = req.body.date;
+    const spl = "update user set date = ?,name = ?,surname = ?,id_card = ?,address = ?,id_login =:? ";
+
+    con.query(sql, [date, name, surname, id_card, address, id_login], function () {
+    });
+});
+
+
+// const date = req.body.date;
 // const information = req.body.information;
 // const id_login = req.body.id_login ;
 // const type = req.body.type;
@@ -328,17 +328,26 @@ con
 // });
 
 //<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< canceljoin activity >>>>>>>>>>>>>>>>>>>>>>>>>>>
-app.delete("/cancel_joinactivity", function (req, res) {
- 
-  
-    
- });
+app.put("/cancel_joinactivity", function (req, res) {
+    const id_join = req.body.id_join
+    const join_status = req.body.join_status
 
- //<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< cancel approve >>>>>>>>>>>>>>>>>>>>>>>>>>>
-app.delete("/cancel_approveinfo", function (req, res) {
-    
-  
- });
+    const sql = "UPDATE oldactive.join SET join_status = ? WHERE id_join = ? "
+    con.query(sql, [join_status,id_join ], function (err, result, fields) {
+    });
+
+
+});
+
+//<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< cancel approve >>>>>>>>>>>>>>>>>>>>>>>>>>>
+app.put("/cancel_approveinfo", function (req, res) {
+    const approve_status = req.body.approve_status;
+    const old_id = req.body.old_id;
+    const sql = "UPDATE oldactive.old_info SET Approve_status = ? WHERE `id_old` = ?"
+    con.query(sql, [approve_status,old_id],function(err,result, fields){
+
+    });
+});
 
 // <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< start port >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
